8a8fea612f6781fe07a91c2e2fd0d62293d4fd6b
THE CODING ZONE <sweetsanjeevkumar96@gmail.com>
2020-05-12 20:19:30 +0530

    rules for user define  variables writing

--- hello.sh
+++ hello.sh
@@ -1,31 +1,17 @@
-#! /bin/bash
+rules for write variables
 
-# The sign #! is called she-bang and is written at top of the script. It passes instruction to program /bin/sh.
+To define UDV use following syntax
+Syntax:
+variable name=value
 
+'value' is assigned to given 'variable name' and Value must be on right side = sign.
 
-# to run script  " ./filename.sh"  if it show permission error then use chmod +x filename
-echo "hello world"
+Example:
+no=10# this is ok
+10=no# Error, NOT Ok, Value must be on right side of = sign.
+To define variable called 'vech' having value Bus
+vech=Bus
+To define variable called n having value 10
+n=10
 
-# variable in shell scripting
-# In Linux (Shell), there are two types of variable:
-# (1) System variables - Created and maintained by Linux itself. This type of variable defined in CAPITAL LETTERS.
-# (2) User defined variables (UDV) - Created and maintained by user. This type of variable defined in lower letters.
-
-#  UDV
-# do not use trailing space in =
-var1=rohit
-var2=kumar
-echo $var1 $var2
-
-# system variables
-
-echo our shell name is $BASH
-echo our bash version is $BASH_VERSION
-echo $HOME
-echo $PWD
-echo $COLUMNS
-echo $LOGNAME
-echo $PATH
-
-
-# for more information read freeos.com
+variable name should not start with number
