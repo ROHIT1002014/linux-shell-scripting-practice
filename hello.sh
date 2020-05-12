d0e770e6774c5ca788de476d2fa5c0c3be053934
THE CODING ZONE <sweetsanjeevkumar96@gmail.com>
2020-05-12 20:16:11 +0530

    do practice of variables

--- hello.sh
+++ hello.sh
@@ -1,7 +1,31 @@
-#! /bin/bash    # The sign #! is called she-bang and is written at top of the script. It passes instruction to program /bin/sh.
+#! /bin/bash
+
+# The sign #! is called she-bang and is written at top of the script. It passes instruction to program /bin/sh.
 
 
 # to run script  " ./filename.sh"  if it show permission error then use chmod +x filename
-
-
 echo "hello world"
+
+# variable in shell scripting
+# In Linux (Shell), there are two types of variable:
+# (1) System variables - Created and maintained by Linux itself. This type of variable defined in CAPITAL LETTERS.
+# (2) User defined variables (UDV) - Created and maintained by user. This type of variable defined in lower letters.
+
+#  UDV
+# do not use trailing space in =
+var1=rohit
+var2=kumar
+echo $var1 $var2
+
+# system variables
+
+echo our shell name is $BASH
+echo our bash version is $BASH_VERSION
+echo $HOME
+echo $PWD
+echo $COLUMNS
+echo $LOGNAME
+echo $PATH
+
+
+# for more information read freeos.com
