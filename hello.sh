ee8752f5eef849fad4dd623c142008abe675622d
THE CODING ZONE <sweetsanjeevkumar96@gmail.com>
2020-05-12 19:30:47 +0530

    first script of linux shell

---
+++ hello.sh
@@ -0,0 +1,7 @@
+#! /bin/bash    # The sign #! is called she-bang and is written at top of the script. It passes instruction to program /bin/sh.
+
+
+# to run script  " ./filename.sh"  if it show permission error then use chmod +x filename
+
+
+echo "hello world"
