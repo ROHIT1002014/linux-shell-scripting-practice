3363ac4d9d034a1e46853fc1e1289fc831bb75ad
THE CODING ZONE <sweetsanjeevkumar96@gmail.com>
2020-05-13 00:58:43 +0530

    Added if else condition

--- hello.sh
+++ hello.sh
@@ -1,7 +1,54 @@
-#! /bin/bash    # The sign #! is called she-bang and is written at top of the script. It passes instruction to program /bin/sh.
+#! /bin/bash
 
+# If given condition is true then command1 is executed otherwise command2 is executed.
+# if [ expression ];
 
-# to run script  " ./filename.sh"  if it show permission error then use chmod +x filename
+#       then
 
+#             statements
 
-echo "hello world"
+#       elif [ expression ];
+
+#       then
+
+#             statements
+
+#       else
+
+#             statements
+
+#       fi
+
+# Put spaces after [ and before ], and around the operators and operands.
+# Number Comparisons:
+
+# -eq - is equal to - if [ "$a" -eq "$b" ]
+
+# -ne - is not equal to - if [ "$a" -ne "$b" ]
+
+# -gt - is greater than - if [ "$a" -gt "$b" ]
+
+# -ge - is greater than or equal to - if [ "$a" -ge "$b" ]
+
+# -lt - is less than - if [ "$a" -lt "$b" ]
+
+# -le - is less than or equal to - if [ "$a" -le "$b" ]
+
+# < - is less than - (("$a" < "$b"))
+
+# <= - is less than or equal to - (("$a" <= "$b"))
+
+# > - is greater than - (("$a" > "$b"))
+
+# >= - is greater than or equal to - (("$a" >= "$b"))
+
+echo -n 'enter the no. between 1<x<10'
+
+read num
+
+if [ $num -lt 10 ]
+  then
+  echo 'number is less then 10'
+else
+  echo 'number is greater then 10'
+fi
