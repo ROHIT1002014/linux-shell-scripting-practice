f8d0c9708d682e6e158e1241d19184ae737fadd7
THE CODING ZONE <sweetsanjeevkumar96@gmail.com>
2020-05-13 17:47:49 +0530

    for loop in shell script

--- chessboard.sh
+++ chessboard.sh
@@ -1,8 +1,24 @@
 #! /bin/bash
 
-# read file content
+# FOR LOOP
 
-cat hello.sh| while read p
+for i in 1 2 3 4 5
 do
-  echo $p
+  echo $i
+done
+echo "another loop output"
+echo ${BASH_VERSION}
+for i in {1..10..2} # {START..EAND..INCREMENT} useful in bash version > 4.0
+do
+  echo $i
+done
+echo "another loop output"
+for i in {1..10}
+do
+  echo $i
+done
+echo "another loop output"
+for (( i=0;i<5;i++))
+do
+echo $i
 done
