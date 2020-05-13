55313ba3e928add536b719f59662ed767cd24135
THE CODING ZONE <sweetsanjeevkumar96@gmail.com>
2020-05-13 18:22:31 +0530

    selected loop for taking choice and give corresponding output

--- chessboard.sh
+++ chessboard.sh
@@ -1,81 +1,92 @@
 #! /bin/bash
 
-# FOR LOOP WITH COMMANDS
+# SELECT LOOP WITH COMMANDS
 
-# for command in list_of_commands
+# select varName in list
 # do
-#   $command
+#     command1
+#     command2
+#     ....
+#     ......
+#     commandN
 # done
 
-# for VARIABLE in 1 2 3 4 5 .. N
+# select name in rohit sanjeev dolly raman
 # do
-# 	command1
-# 	command2
-# 	commandN
+#   echo "$name selected"
 # done
 
-# OR
+# OUTPUT
+# 1) rohit
+# 2) sanjeev
+# 3) dolly
+# 4) raman
+# #? 3
+# dolly selected
+# #? 2
+# sanjeev selected
+# #? 4
+# raman selected
+# #? 3
+# dolly selected
 
-# for VARIABLE in file1 file2 file3
+# SELECT LOOP WITH CASES
+# select varName in list
 # do
-# 	command1 on $VARIABLE
-# 	command2
-# 	commandN
+# 	case $varName in
+# 		pattern1)
+# 			command1;;
+# 		pattern2)
+# 			command2;;
+# 		pattern1)
+# 			command3;;
+# 		*)
+# 			echo "Error select option 1..3";;
+# 	esac
 # done
 
-# OR
+echo "Enter the space shuttle to get quick information : "
 
-# for OUTPUT in $(Linux-Or-Unix-Command-Here)
-# do
-# 	command1 on $OUTPUT
-# 	command2 on $OUTPUT
-# 	commandN
-# done
-
-# OR
-
-# for (( EXP1; EXP2; EXP3 ))
-# do
-# 	command1
-# 	command2
-# 	command3
-# done
-
-# for I in 1 2 3 4 5
-# do
-#   statements1      #Executed for all values of ''I'', up to a disaster-condition if any.
-#   statements2
-#   if (disaster-condition)
-#   then
-# 	break       	   #break the loop.
-#   fi
-#   statements3          #While good and, no disaster-condition.
-# done
-
-
-# for I in 1 2 3 4 5
-# do
-#   statements1      #Executed for all values of ''I'', up to a disaster-condition if any.
-#   statements2
-#   if (condition)
-#   then
-# 	continue   #Go to next iteration of I in the loop and skip statements3
-#   fi
-#   statements3
-# done
-
-
-for command in ls pwd date
+select shuttle in columbia endeavour challenger discovery atlantis enterprise pathfinder
 do
-  echo '---------------commands-----------------'
-  $command
-done
-
-for item in * # (* is wildcard ) iterate overs files present in current directory
-do
-  echo "-----------------------commands-------------------"
-  if [ -d $command ]  # check weather it is directory or not
-  then
-    echo $item
-  fi
+	case $shuttle in
+		columbia)
+			echo "--------------"
+			echo "Space Shuttle Columbia was the first spaceworthy space shuttle in NASA's orbital fleet."
+			echo "--------------"
+			;;
+		endeavour)
+			echo "--------------"
+			echo "Space Shuttle Endeavour is one of three currently operational orbiters in the Space Shuttle."
+			echo "--------------"
+			;;
+		challenger)
+			echo "--------------"
+		    echo "Space Shuttle Challenger was NASA's second Space Shuttle orbiter to be put into service."
+			echo "--------------"
+			;;
+		discovery)
+			echo "--------------"
+			echo "Discovery became the third operational orbiter, and is now the oldest one in service."
+			echo "--------------"
+			;;
+		atlantis)
+			echo "--------------"
+			echo "Atlantis was the fourth operational shuttle built."
+			echo "--------------"
+			;;
+		enterprise)
+			echo "--------------"
+			echo "Space Shuttle Enterprise was the first Space Shuttle orbiter."
+			echo "--------------"
+			;;
+		pathfinder)
+			echo "--------------"
+			echo "Space Shuttle Orbiter Pathfinder is a Space Shuttle simulator made of steel and wood."
+			echo "--------------"
+			;;
+		*)
+			echo "Error: Please try again (select 1..7)!"
+			;;
+	esac
 done
