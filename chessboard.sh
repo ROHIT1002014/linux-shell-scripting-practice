77942a28ad1c7c513c72df3288c058c4d4daf09c
THE CODING ZONE <sweetsanjeevkumar96@gmail.com>
2020-05-13 15:29:57 +0530

    read character of files

--- chessboard.sh
+++ chessboard.sh
@@ -1,23 +1,8 @@
-for (( i=0; i<=9; i++ ))
-do
-  for ((j=0;j<=9;j++))
-  do
-    tot=`expr $i + $j`
-    tmp=`expr $tot % 2`
-    if [ $tmp -eq 0 ]; then
-    echo -e -n "\033[47m "
-    sleep .15 # it will sleep for .15 second
-    else
-        echo -e -n "\033[40m "
-        sleep .15
-    fi
-  done
-  echo -e -n "\033[40m"
-  echo ""
-  sleep .15
-done
+#! /bin/bash
 
-for ((i=0; i<2; i++))
+# read file content
+
+cat hello.sh| while read p
 do
-  gnome-terminal &
+  echo $p
 done
